108985427e2b2e8b3d878076b49b408f74374302
Matheus Franklin <matheus.franklin.campos@gmail.com>
2024-08-12 19:42:06 -0300

    timer

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Bem vindo ao projeto Timer!
+
+Projeto elaborado para potencializar o seu foco nos estudos.
+
+O Timer conta com modo noturno, música ambiente de fundo e alarme ao fim no tempo estipulado.
---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/start.mp3')
+
+export const timeOut = new Audio('./assets/time-out.mp3')
+
+export const music = new Audio('./assets/music.mp3')
+
+music.loop = true
